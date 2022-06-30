b3dcf939414df80513376f6bcd7147772772e449
davidadebayo17108247@gmail.com <davidadebayo17108247.com>
2022-06-30 10:19:44 +0100

    step-31-and-Step-32.

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart' show ReadContext;
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/extensions/buildcontext/loc.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/services/auth/bloc/auth_event.dart';
 import 'package:mynotes/services/auth/bloc/authbloc.dart';
@@ -12,6 +13,10 @@ import 'package:mynotes/utility/dialogs/logout_dialog.dart';
 import 'package:mynotes/views/notes/note_list_view.dart';
 
 import '../../enums/menu_action.dart';
+
+extension Count<T extends Iterable> on Stream<T>{
+  Stream<int> get getLength => map((event) => event.length);
+}
 
 class NotesView extends StatefulWidget {
   const NotesView({Key? key}) : super(key: key);
@@ -38,7 +43,18 @@ class _NotesViewState extends State<NotesView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Your Notes'),
+        title: StreamBuilder(
+          stream: _notesService.allNotes(ownerUserId: userId).getLength,
+          builder: (context, AsyncSnapshot<int> snapshot) {
+            if (snapshot.hasData){
+              final  noteCount = snapshot.data ?? 0;
+              final text = context.loc.notes_title(noteCount);
+              return Text(text);
+            }else{
+              return const Text('');
+            }
+          },
+          ),
         actions: [
           IconButton(onPressed: (){
             Navigator.of(context).pushNamed(createOrUpdateNoteRoute);   
